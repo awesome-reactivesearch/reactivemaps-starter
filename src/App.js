cbb04944f9071994055b7e07edf10a64dc688bfa
dhruvdutt <dhruvdutt.jadhav@gmail.com>
2018-04-18 00:50:22 +0530

    Add components code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { ReactiveBase, SingleList } from '@appbaseio/reactivesearch';
+import { ReactiveMap } from '@appbaseio/reactivemaps';
+
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+		return (
+			<ReactiveBase
+				app="earthquake"
+				credentials="OrXIHcgHn:d539c6e7-ed14-4407-8214-c227b0600d8e"
+				type="places"
+				mapKey="AIzaSyBQdVcKCe0q_vOBDUvJYpzwGpt_d_uTj4Q"
+			>
+				<div
+					style={{
+						width: '100%',
+						display: 'flex',
+						flexDirection: 'row',
+						justifyContent: 'space-between'
+					}}
+				>
+					<SingleList
+						title="Places"
+						componentId="places"
+						dataField="place.raw"
+						size={50}
+						showSearch={true}
+						style={{
+							width: '25%',
+							padding: 20
+						}}
+					/>
+
+					<ReactiveMap
+						componentId="map"
+						dataField="location"
+						react={{
+							and: "places"
+						}}
+						onData={result => ({
+							label: result.mag
+						})}
+						style={{
+							width: '70%'
+						}}
+					/>
+				</div>
+			</ReactiveBase>
+		);
+	}
 }
 
 export default App;
